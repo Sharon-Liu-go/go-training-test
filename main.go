fe9a9a4302897f2535dde5b05b61a7f5f2089a21
Sharon-liu-go <innovate72095@gmail.com>
2022-11-02 23:50:50 +0800

    chore: crawler, not to get if image's data-src is empty

--- main.go
+++ main.go
@@ -30,9 +30,11 @@ func collyUseTemplate() {
 	})
 	//OnResponse如果收到的內容是HTML ,則在之後調用
 	collector.OnHTML("img", func(element *colly.HTMLElement) {
-		// todo 解析html內容
-		count++
-		fmt.Println("顯示:", element.Attr("data-src"))
+			// 解析html內容
+		if element.Attr("data-src") != "" { //查看要查詢的該2個網頁，其圖片網址都塞在data-src裡，因此解析對象用html裡有屬性data-src的，並且有值不要空的
+			count++
+			fmt.Println("顯示:", element.Attr("data-src"))
+		}
 	})
 
 	// url：請求具體的地址
